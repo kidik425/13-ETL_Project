6cc988a1e5947696921eecfe7b426e7b5187e693
Steve Walker <bsteven.walker@gmail.com>
2021-06-13 17:13:34 -0400

    Added DB Creation Script

---
+++ DataModel/DBCreation.sql
@@ -0,0 +1,12 @@
+-- Database: etl_project
+
+-- DROP DATABASE etl_project;
+
+CREATE DATABASE etl_project
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
